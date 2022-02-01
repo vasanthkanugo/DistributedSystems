1b402983a6b2c07a43691fe900a5a5bb4dfed08a
vasanth.kanugo <vasanth.kanugo@gmail.com>
2022-02-01 14:09:52 -0700

    initial commit

---
+++ Lab01/ClientSideSeller/string_util.py
@@ -0,0 +1,9 @@
+# Response codes and responses
+error = {'response_code': 400,
+                'error_message':''}
+ok = {'response_code': 200,
+      'response_message': 'Success; OK'}
+
+
+missing_header_error = 'missing header or invalid header'
+
