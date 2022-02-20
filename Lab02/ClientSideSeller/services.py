26a1152882853e37c122fe0508983e27cf73b56c
Arpita Ambavane <arpita.ambavane@colorado.edu>
2022-02-20 13:46:32 -0700

    services file cleanup

--- Lab02/ClientSideSeller/services.py
+++ Lab02/ClientSideSeller/services.py
@@ -1,6 +1,4 @@
 from util import string_util, Util, db_util
-from datetime import datetime as dt
-from suds.client import Client
 import seller_db_util
 
 # Get list of all items in the database
@@ -91,25 +89,3 @@ def login_seller_account(data, db_name=None):
         return login_details
     login_details = login_details['items'][0]
     return login_details
-
-
-
-
-
-# Update the item list
-# def put_or_update(data):
-#     updated_list = db_util.read_db(db_name=db_util.item_db)
-#     print(updated_list)
-#     if 'item_id' not in data['Body'] or 'quantity' not in data['Body']:
-#         return string_util.missing_request_parameters.format(request_parameters='item_id or quantity')
-#     body = data['Body']
-#     if body['item_id'] not in updated_list:
-#         return string_util.item_missing.format(item_id=body['item_id'])
-#     item = updated_list[body['item_id']]
-#     item['quantity'] = body['quantity']
-#     if item['quantity'] == 0:
-#         item = None
-#     response_message = db_util.write_db(db_name=db_util.item_db, entries={body['item_id']: item})
-#     print(response_message)
-#     print("put/update db")
-#     return None
